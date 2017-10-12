c2fe31813544da3f5edba60fde63c82d0d929141
Christoph Reimers <christoph@superservice-international.com>
2017-10-12 17:57:36 +0200

    render only 100 cells

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 import ReactMapboxGl, { GeoJSONLayer, Marker } from "react-mapbox-gl";
 
-import { shape } from './../../data/geo'
-import geojson from './../../data/parcelen.geojson'
+import { data } from './../../data/parcelen.js'
 
 const token = 'pk.eyJ1IjoiY3JlaW1lcnMiLCJhIjoiY2o4b2d3ZHNxMDJiczMycDYzdXgyaHNnYiJ9.lv43SVuUini5Qm6cCLpfJw'
 
@@ -25,11 +24,11 @@ export default class Map extends Component {
       <MapboxMap
         style="mapbox://styles/mapbox/streets-v8"
         containerStyle={{height: '100vh', width: '100%'}}
-        center={[4.9944, 52.2528]}
-        zoom={[12]}
+        center={[4.9944, 52.2180]}
+        zoom={[14]}
       >
         <GeoJSONLayer
-          data={geojson}
+          data={data}
           fillPaint={{'fill-color': fillColor}}
           fillOnClick={this.handleOnClick}
         >
